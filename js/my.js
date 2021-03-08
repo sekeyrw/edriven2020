8d2d1f0239c1e066c4a6865fd663d166bea973a8
sekeyrw <71113187+sekeyrw@users.noreply.github.com>
2021-03-08 19:35:08 +0800

    final commit

--- js/my.js
+++ js/my.js
@@ -1,267 +1,53 @@
-var m, d;
-m = prompt("Month 1-12")
-d = prompt("Start Day 1-7")
+function mainForm(){
+    var subtotal, tax, total, amountPaid, change, percentToGet, pay;
+    pay = document.getElementById("pay")
+    percentToGet = 12;
+    subtotal = document.getElementById("subtotal").value;
+    tax = Number((percentToGet / 100) * subtotal);
+    total =  Number(subtotal) + Number(tax);
 
-function createCalendar(elem, month, dayOfWeek) {
-    const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    const round = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    amountPaid = Number(total);  
+    change = Number(amountPaid-total);
 
-    let data = []
-
-    let stringMonth = ''
-    if (month == 1)
-        stringMonth = 'JANUARY'
-    else if (month == 2)
-        stringMonth = 'FEBRUARY'
-    else if (month == 3)
-        stringMonth = 'MARCH'
-    else if (month == 4)
-        stringMonth = 'APRIL'
-    else if (month == 5)
-        stringMonth = 'MAY'
-    else if (month == 6)
-        stringMonth = 'JUNE'
-    else if (month == 7)
-        stringMonth = 'JULY'
-    else if (month == 8)
-        stringMonth = 'AUGUST'
-    else if (month == 9)
-        stringMonth = 'SEPTEMBER'
-    else if (month == 10)
-        stringMonth = 'OCTOBER'
-    else if (month == 11)
-        stringMonth = 'NOVEMBER'
-    else if (month == 12)
-        stringMonth = 'DECEMBER'
-    else {
-        stringMonth = "Invalid Input"
+    if(subtotal > 99){
+        document.getElementById("tax").value = tax.toFixed(2);
+        document.getElementById("total").value = total.toFixed(2);
+        document.getElementById("amountpaid").value = amountPaid.toFixed(2);
+        document.getElementById("amountpaid").disabled = false;
+        document.getElementById("amountpaid").min = total.toFixed(2);
+        document.getElementById("change").value = change.toFixed(2);
+        pay.style.backgroundColor = "#5de95d";
+        pay.disabled = false;
     }
-
-    let table = `<table><tr><td colspan = "7" align = "center" id="month">${stringMonth}</tr></td><tr><th>SUN</th><th>MON</th><th>TUE</th><th>WED</th><th>THU</th><th>FRI</th><th>SAT</th></tr>`
-    let counter = 1
-    let calendarDays = 1
-    let weekendCounter = 0
-
-    if (month > 12 || dayOfWeek > 7) {
-        elem.innerHTML = ("<h2>Invalid Input</h2>")
+    else{
+        document.getElementById("tax").value = "";
+        document.getElementById("total").value = "";
+        document.getElementById("amountpaid").value = "";
+        document.getElementById("amountpaid").disabled = true;
+        document.getElementById("amountpaid").min = "";
+        document.getElementById("change").value = "";
+        pay.style.backgroundColor = "#ff0000";
+        pay.disabled = true;
     }
-    else {
+};
 
-        for (let i = 0; i < days[month - 1]; i++) {
-            data[i] = round[(dayOfWeek + i) % 7]
+function changeVal(){
+    var subtotal, tax, total, amountPaid, change;
+    subtotal = document.getElementById("subtotal").value;
+    tax = document.getElementById("tax").value
+    total =  document.getElementById("total").value
+
+    amountPaid = document.getElementById("amountpaid").value 
+    change = Number(amountPaid-total);
+
+    document.getElementById("change").value = change.toFixed(2);
+
+        if(change > "-1"){
+            pay.style.backgroundColor = "#5de95d";
+            pay.disabled = false;
         }
-
-        if (days[month - 1] != 28) {
-            if (days[month - 1] == 30) {
-                if (dayOfWeek > 6) {
-                    for (let i = 0; i < 42; i++) {
-                        if (weekendCounter == 0) {
-                            table += '<tr>'
-                        }
-
-                        if (counter <= dayOfWeek - 1) {
-                            table += "<td></td>"
-                            counter++
-                        }
-
-                        else if (calendarDays > days[month - 1])
-                            table += "<td></td>"
-
-                        else {
-                            if (calendarDays <= days[month - 1]) {
-                                table += `<td>${calendarDays}</td>`
-                                calendarDays++
-                            }
-                            else
-                                table += "<td></td>"
-                        }
-
-                        if (weekendCounter == 6) {
-                            table += '</tr>'
-                            weekendCounter = 0
-                        }
-                        else {
-                            weekendCounter++
-                        }
-                    }
-                }
-                else {
-                    for (let i = 0; i < 35; i++) {
-                        if (weekendCounter == 0) {
-                            table += '<tr>'
-                        }
-
-                        if (counter <= dayOfWeek - 1) {
-                            table += "<td></td>"
-                            counter++
-                        }
-
-                        else if (calendarDays > days[month - 1])
-                            table += "<td></td>"
-
-                        else {
-                            if (calendarDays <= days[month - 1]) {
-                                table += `<td>${calendarDays}</td>`
-                                calendarDays++
-                            }
-                            else
-                                table += "<td></td>"
-                        }
-
-                        if (weekendCounter == 6) {
-                            table += '</tr>'
-                            weekendCounter = 0
-                        }
-                        else {
-                            weekendCounter++
-                        }
-                    }
-
-                }
-
-            }
-            else if (days[month - 1] == 31) {
-                if (dayOfWeek > 5) {
-                    for (let i = 0; i < 42; i++) {
-                        if (weekendCounter == 0) {
-                            table += '<tr>'
-                        }
-
-                        if (counter <= dayOfWeek - 1) {
-                            table += "<td></td>"
-                            counter++
-                        }
-
-                        else if (calendarDays > days[month - 1])
-                            table += "<td></td>"
-
-                        else {
-                            if (calendarDays <= days[month - 1]) {
-                                table += `<td>${calendarDays}</td>`
-                                calendarDays++
-                            }
-                            else
-                                table += "<td></td>"
-                        }
-
-                        if (weekendCounter == 6) {
-                            table += '</tr>'
-                            weekendCounter = 0
-                        }
-                        else {
-                            weekendCounter++
-                        }
-                    }
-                }
-                else {
-                    for (let i = 0; i < 35; i++) {
-                        if (weekendCounter == 0) {
-                            table += '<tr>'
-                        }
-
-                        if (counter <= dayOfWeek - 1) {
-                            table += "<td></td>"
-                            counter++
-                        }
-
-                        else if (calendarDays > days[month - 1])
-                            table += "<td></td>"
-
-                        else {
-                            if (calendarDays <= days[month - 1]) {
-                                table += `<td>${calendarDays}</td>`
-                                calendarDays++
-                            }
-                            else
-                                table += "<td></td>"
-                        }
-
-                        if (weekendCounter == 6) {
-                            table += '</tr>'
-                            weekendCounter = 0
-                        }
-                        else {
-                            weekendCounter++
-                        }
-                    }
-
-                }
-            }
+        else{
+            pay.style.backgroundColor = "#ff0000";
+            pay.disabled = true;
         }
-        else {
-            if (dayOfWeek >= 2) {
-                for (let i = 0; i < 35; i++) {
-                    if (weekendCounter == 0) {
-                        table += '<tr>'
-                    }
-
-                    if (counter <= dayOfWeek - 1) {
-                        table += "<td></td>"
-                        counter++
-                    }
-
-                    else if (calendarDays > days[month - 1])
-                        table += "<td></td>"
-
-                    else {
-                        if (calendarDays <= days[month - 1]) {
-                            table += `<td>${calendarDays}</td>`
-                            calendarDays++
-                        }
-                        else
-                            table += "<td></td>"
-                    }
-
-                    if (weekendCounter == 6) {
-                        table += '</tr>'
-                        weekendCounter = 0
-                    }
-                    else {
-                        weekendCounter++
-                    }
-                }
-            }
-            else {
-                for (let i = 0; i < 28; i++) {
-                    if (weekendCounter == 0) {
-                        table += '<tr>'
-                    }
-
-                    if (counter <= dayOfWeek - 1) {
-                        table += "<td></td>"
-                        counter++
-                    }
-
-                    else if (calendarDays > days[month - 1])
-                        table += "<td></td>"
-
-                    else {
-                        if (calendarDays <= days[month - 1]) {
-                            table += `<td>${calendarDays}</td>`
-                            calendarDays++
-                        }
-                        else
-                            table += "<td></td>"
-                    }
-
-                    if (weekendCounter == 6) {
-                        table += '</tr>'
-                        weekendCounter = 0
-                    }
-                    else {
-                        weekendCounter++
-                    }
-                }
-
-            }
-        }
-
-        table += '</table>'
-
-        elem.innerHTML = table;
-        return data
-
-    }
-}
-
-createCalendar(main, m, d)
+};
